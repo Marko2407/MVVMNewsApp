de54d013ccf92946fc3347131497d07ac09157aa
Marko Vukosav <marko.vukosav@rimac-automobili.com>
2021-08-24 16:36:14 +0200

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "a30d783aa31e476e94a35f3c88b66810"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
